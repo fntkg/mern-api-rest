c179a55854d461095788ca9c1c1baa56f42a7c64
German Garces <757024@unizar.es>
2022-03-28 21:00:20 +0200

    fix: var assign when creating a message

--- api/messages/messageController.js
+++ api/messages/messageController.js
@@ -17,40 +17,26 @@ exports.create = (req, res) => {
     if (req.user !== req.params.username) return res.sendStatus(403)
     User.findOne({username: req.user}).
         exec(function (err, user){
-        Message.create({
-            _id: new mongoose.Types.ObjectId(),
-            user: user._id,
-            original_message: req.body.id_comment,
-            content: req.body.content,
-        }, function (err, message){
-            if (err) return res.sendStatus(400)
-            res.status(201).send({id: message.id})
+        User.findOne({username: req.user}, 'id').exec(function (err, user){
+            Message.create({
+                _id: new mongoose.Types.ObjectId(),
+                user: user.id,
+                original_message: req.body.id_comment,
+                content: req.body.content,
+            }, function (err, message){
+                if (err) return res.sendStatus(400)
+                console.log('>> Mensaje creado: ' + message)
+                res.status(201).send({id: message.id})
+            })
         })
     })
-
 }
 
 exports.findOne = (req, res) => {
     if (req.user !== req.params.username) return res.sendStatus(403)
-    Message.findOne({_id: req.params.id}, '_id user date content likes shared numOfComments comments original_message').
-        populate('user', 'username name avatar').
-        populate({
-            path: 'original_message',
-            projection: '_id date content likes shared numOfComments user',
-            populate: {
-                path: 'user',
-                projection: 'username name avatar'
-            }
-        }).
-        populate({
-            path: 'comments',
-            projection: '_id date content likes shared numOfComments user',
-            populate: {
-                path: 'user',
-                projection: 'username name avatar'
-            }
-        }).exec(function (err, message){
+    Message.findOne({_id: req.params.id}, ).exec(function (err, message){
         if (err) return res.sendStatus(400)
+        console.log('Retrieved message: ' + message)
         return res.status(200).send(message)
     })
 }
